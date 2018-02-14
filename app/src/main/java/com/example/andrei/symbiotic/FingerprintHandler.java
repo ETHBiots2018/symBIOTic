274c6c7c22ce23e7f35146bc9d84b5607b18d185
Andrei Ursache <andreiursache94@gmail.com>
2018-02-14 17:59:39 +0100

    [Android] Fingerprint Done.

--- app/src/main/java/com/example/andrei/symbiotic/FingerprintHandler.java
+++ app/src/main/java/com/example/andrei/symbiotic/FingerprintHandler.java
@@ -21,12 +21,22 @@ package com.example.andrei.symbiotic;
  */
 
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.fingerprint.FingerprintManager;
 import android.os.Build;
 import android.os.CancellationSignal;
 import android.support.annotation.RequiresApi;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.Signature;
+import java.security.SignatureException;
 
 /**
  * Created by francesco on 29/11/16.
@@ -36,7 +46,69 @@ import android.widget.Toast;
 public class FingerprintHandler extends FingerprintManager.AuthenticationCallback {
 
     private Context c;
+
     public FingerprintHandler(Context c) {
+        this.c = c;
+
+
+    }
+
+    @Override
+    public void onAuthenticationError(int errorCode, CharSequence errString) {
+        super.onAuthenticationError(errorCode, errString);
+        Toast.makeText(c, "Auth ERROR.", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onAuthenticationHelp(int helpCode, CharSequence helpString) {
+        super.onAuthenticationHelp(helpCode, helpString);
+
+    }
+
+    @Override
+    public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
+        super.onAuthenticationSucceeded(result);
+
+        Signature sig = result.getCryptoObject().getSignature();
+
+
+        Toast.makeText(c, "Authenticated!", Toast.LENGTH_SHORT).show();
+
+
+        Intent intent = new Intent(c, Home.class);
+        String userId = "id";
+        intent.putExtra("id", userId);
+        c.startActivity(intent);
+
+    }
+
+    @Override
+    public void onAuthenticationFailed() {
+        super.onAuthenticationFailed();
+
+        Toast.makeText(c, "Auth Failed!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void doAuth(FingerprintManager manager, FingerprintManager.CryptoObject obj) {
+
+        CancellationSignal signal = new CancellationSignal();
+
+        Toast.makeText(c, "Scan your finger!", Toast.LENGTH_SHORT).show();
+
+        try {
+            manager.authenticate(obj, signal, 0, this, null);
+        } catch (SecurityException sce) {
+
+        }
+    }
+}
+
+@RequiresApi(api = Build.VERSION_CODES.M)
+class FingerprintHandlerRegister extends FingerprintManager.AuthenticationCallback {
+
+    private Context c;
+
+    public FingerprintHandlerRegister(Context c) {
         this.c = c;
     }
 
@@ -55,27 +127,32 @@ public class FingerprintHandler extends FingerprintManager.AuthenticationCallbac
     @Override
     public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
         super.onAuthenticationSucceeded(result);
-        Toast.makeText(c, "Auth OK.", Toast.LENGTH_SHORT).show();
 
+        Toast.makeText(c, "User Registered!", Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(c, Home.class);
+        String userId = "id";
+        intent.putExtra("id", userId);
+        c.startActivity(intent);
 
     }
 
     @Override
     public void onAuthenticationFailed() {
         super.onAuthenticationFailed();
-        Toast.makeText(c, "Auth Failed.", Toast.LENGTH_SHORT).show();
 
     }
 
     public void doAuth(FingerprintManager manager, FingerprintManager.CryptoObject obj) {
+
+
         CancellationSignal signal = new CancellationSignal();
-        Toast.makeText(c, "Do Auth.", Toast.LENGTH_SHORT).show();
+
+        Toast.makeText(c, "Scan your finger!", Toast.LENGTH_SHORT).show();
 
         try {
             manager.authenticate(obj, signal, 0, this, null);
         } catch (SecurityException sce) {
-            Toast.makeText(c, "Auth Exception", Toast.LENGTH_SHORT).show();
-
         }
     }
 }
